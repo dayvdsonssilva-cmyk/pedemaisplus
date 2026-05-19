afe1ca2a6b16ae2ed1c2838f359127e592acad88
dayvdsonssilva-cmyk <dayvdson.s.silva@gmail.com>
2026-05-19 11:37:27 -0300

    Update create-preference.js

--- api/create-preference.js
+++ api/create-preference.js
@@ -53,5 +53,5 @@ export default async function handler(req, res) {
   } catch (error) {
     console.error("Server error:", error);
     return res.status(500).json({ error: "Erro interno do servidor" });
-  }
+  } 
 }
